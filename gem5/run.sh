f52d8ed7fe8b4d57a8567f5bc6191fc201526327
RohSiHyun <49300588+RohSiHyun@users.noreply.github.com>
2023-06-22 23:27:58 +0900

    Update run.sh

--- gem5/run.sh
+++ gem5/run.sh
@@ -3,5 +3,5 @@
 
 ./build/RISCV/gem5.opt ./configs/example/se.py --cmd=progs/pim_test --cpu-type=TimingSimpleCPU --mem-type=Ramulator --ramulator-config=../ramulator/configs/DDR4-config.cfg > debug_pim
 
-./build/RISCV/gem5.opt ./configs/example/se.py --cmd=./progs/nop_test --cpu-type=TimingSimpleCPU --mem-type=Ramulator --ramulator-config=../ramulator/configs/DDR4-config.cfg > debug_mem
+./build/RISCV/gem5.opt ./configs/example/se.py --cmd=./progs/mem_test --cpu-type=TimingSimpleCPU --mem-type=Ramulator --ramulator-config=../ramulator/configs/DDR4-config.cfg > debug_mem
 
